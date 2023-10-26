183d0206916a3daed92aa0501d3f58ce9a8c8ca3
AumaidKh <murtaza@fourthfrontier.com>
2023-10-26 20:14:27 +0530

    Added App Center SDK

--- app/src/main/java/com/hopcape/torchify/MainActivity.kt
+++ app/src/main/java/com/hopcape/torchify/MainActivity.kt
@@ -2,25 +2,23 @@ package com.hopcape.torchify
 
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.hopcape.torchify.presentation.screen.HomeScreen
 import com.hopcape.torchify.ui.theme.TorchiFyTheme
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.BuildConfig
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window,false)
+        initAppCenter()
         setContent {
             TorchiFyTheme {
                 HomeScreen(
@@ -36,6 +34,11 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+    }
+
+    private fun initAppCenter(){
+        AppCenter.start(application, "",
+            Analytics::class.java, Crashes::class.java)
     }
 
 }
